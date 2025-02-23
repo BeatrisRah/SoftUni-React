2c4287100106b681ff9de628a4f73f82faae167d
Beatris <beatrisraj7@gmail.com>
2025-02-23 19:00:09 +0200

    fixed bugs

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -2,42 +2,56 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-	const [number, setNumber] = useState('')
+	const [number, setNumber] = useState('0')
 
 	function changeDisplay(e){
 		const newNum = e.target.textContent
-		setNumber(n => n + newNum)
 		
-		
+		if (newNum === "." && number.includes(".")) return;
+
+		if(number == 0){
+			setNumber(newNum)
+		} else {
+			setNumber(n => n + newNum)
+		}
 	} 
 
+	function clearDisplay(){
+		setNumber(0)
+	}
+
+
+	function calculate(e){
+		setNumber(num => eval(num))
+		
+	}
 	return (
 		<>
 			<div className="calculator">
 				<div className="display" id="display">
-					{number === '' ? 0: number}
+					{number}
 				</div>
 				<div className="buttons">
 					<button onClick={changeDisplay}>7</button>
 					<button onClick={changeDisplay}>8</button>
 					<button onClick={changeDisplay}>9</button>
 
-					<button className="operator">/</button>
+					<button className="operator" onClick={clearDisplay}>C</button>
 
 					<button onClick={changeDisplay}>4</button>
 					<button onClick={changeDisplay}>5</button>
 					<button onClick={changeDisplay}>6</button>
-					<button className="operator">*</button>
+					<button className="operator" onClick={changeDisplay}>*</button>
 					<button onClick={changeDisplay}>1</button>
 					<button onClick={changeDisplay}>2</button>
 					<button onClick={changeDisplay}>3</button>
 
-					<button className="operator">-</button>
+					<button className="operator" onClick={changeDisplay}>-</button>
 
 					<button onClick={changeDisplay}>0</button>
 					<button onClick={changeDisplay}>.</button>
-					<button className="operator">=</button>
-					<button className="operator">+</button>
+					<button className="operator" onClick={calculate}>=</button>
+					<button className="operator" onClick={changeDisplay}>+</button>
 				</div>
 			</div>
 		</>
