032748351c43ffff6297885172821170af0c05ea
Beatris <beatrisraj7@gmail.com>
2025-02-23 18:43:03 +0200

    Change display

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,11 +1,47 @@
-
+import { useState } from 'react';
+import './App.css'
 
 function App() {
+	const [number, setNumber] = useState('')
 
-  return (
-    <>
-    </>
-  )
+	function changeDisplay(e){
+		const newNum = e.target.textContent
+		setNumber(n => n + newNum)
+		
+		
+	} 
+
+	return (
+		<>
+			<div className="calculator">
+				<div className="display" id="display">
+					{number === '' ? 0: number}
+				</div>
+				<div className="buttons">
+					<button onClick={changeDisplay}>7</button>
+					<button onClick={changeDisplay}>8</button>
+					<button onClick={changeDisplay}>9</button>
+
+					<button className="operator">/</button>
+
+					<button onClick={changeDisplay}>4</button>
+					<button onClick={changeDisplay}>5</button>
+					<button onClick={changeDisplay}>6</button>
+					<button className="operator">*</button>
+					<button onClick={changeDisplay}>1</button>
+					<button onClick={changeDisplay}>2</button>
+					<button onClick={changeDisplay}>3</button>
+
+					<button className="operator">-</button>
+
+					<button onClick={changeDisplay}>0</button>
+					<button onClick={changeDisplay}>.</button>
+					<button className="operator">=</button>
+					<button className="operator">+</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
