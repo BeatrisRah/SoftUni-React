a0052a369c5ce6b74679ae6a619217e3bb5aba7b
Beatris <beatrisraj7@gmail.com>
2025-02-25 20:22:15 +0200

    Add components to root

--- react-to-do/client/src/main.jsx
+++ react-to-do/client/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import Footer from './Footer.jsx'
+import Section from './Section.jsx'
 
 createRoot(document.getElementById('root')).render(
 		<>
-		<App />
+		<Section />
 		<Footer />
 		</>
 )
