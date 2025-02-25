c91f6fac23e0926a07b21baf27883aab60050177
Beatris <beatrisraj7@gmail.com>
2025-02-25 20:20:35 +0200

    Footer and Spiner Components

--- react-to-do/client/src/Footer.jsx
+++ react-to-do/client/src/Footer.jsx
@@ -3,7 +3,7 @@
 
 export default function Footer(){
     return (
-        <footer class="footer">
+        <footer className="footer">
             <p>Copyright © designed by Mihail Valkov</p>
         </footer>
     )
