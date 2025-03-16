013cd0e54a83ddec38d1ed2e8ce6755be01c2f4e
Beatris <beatrisraj7@gmail.com>
2025-03-16 17:02:56 +0200

    Use user context for sending
    Also create unique id for message

--- react-chat/client/src/hooks/usePostFetch.js
+++ react-chat/client/src/hooks/usePostFetch.js
@@ -1,19 +1,36 @@
 import { useEffect, useState } from "react"
+import { useUser } from "../context/UserContext"
+import {v4 as uuid} from 'uuid'
 
 const chatsUrl = 'http://localhost:3030/jsonstore/messenger'
 
 
 export default function usePostFetch(defaultValuesSeter){
-    const [val, setVal] = useState({content:'', user:'Bett'})
+    const {user, login, logout} = useUser()
+    const [val, setVal] = useState({content:'', user,})
+    const [error, setError] = useState(null)
 
 
     const formSubmiter = (formData) => {
         const content = formData.get('content')
-        setVal(v => ({...v, content:content}))
+        const id = uuid()
+        setVal(v => ({...v, content:content, id}))
 
     }
     useEffect(() => {
-        if(val.content === '') return;
+        if(val.content === '' ) {
+            setError('Please type a message')
+            return
+        };
+
+        if(!user){
+            setError('Plese log in!')
+            return;
+        }
+
+        //if no errors, clear last ones
+        setError(null)
+
         fetch(chatsUrl, {
             method:'POST',
             headers:{
@@ -25,6 +42,6 @@ export default function usePostFetch(defaultValuesSeter){
         .then(data => defaultValuesSeter(c => ([...c, data])))
     }, [val])
 
-    return [formSubmiter]
+    return [formSubmiter, error]
 
 }
