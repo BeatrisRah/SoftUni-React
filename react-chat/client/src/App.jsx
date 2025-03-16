082a5a2c58bb42475ee71822a06ae566038181e5
Beatris <beatrisraj7@gmail.com>
2025-03-16 17:01:56 +0200

    Use context for current user

--- react-chat/client/src/App.jsx
+++ react-chat/client/src/App.jsx
@@ -1,17 +1,34 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import Chatroom from "./components/chat/Chatroom"
+import Navigation from "./components/Navigation"
+import { useState } from "react"
+import { UserContext } from "./context/UserContext"
+
 
 function App() {
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chatroom />} />
-      </Routes>
-    </>
-  )
+	const [user, setUser] = useState(null)
+
+	const login = (userData) => {
+		setUser(userData)
+	}
+
+	const logout = () => {
+		setUser(null)
+	}
+	return (
+		<>
+			<UserContext.Provider value={{user, login, logout}}>
+
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/chat" element={<Chatroom />} />
+				</Routes>
+			</UserContext.Provider>
+		</>
+	)
 }
 
 export default App
--- react-chat/client/src/main.jsx
+++ react-chat/client/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
-import Navigation from './components/Navigation.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Navigation />
-      <App />
+import { UserContext } from './context/UserContext.js'
 
-    </BrowserRouter>
-  </StrictMode>,
+
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<BrowserRouter>
+				<App />
+			
+		</BrowserRouter>
+	</StrictMode>,
 )
