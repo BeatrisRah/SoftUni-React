5b8a62ddedd7b8d5912759fe7466b6eb7fe37a0f
Beatris <beatrisraj7@gmail.com>
2025-04-21 18:05:29 +0300

    App Headers and read.me

--- react-typescript/src/components/Headers.tsx
+++ react-typescript/src/components/Headers.tsx
@@ -6,8 +6,8 @@ import { blueGrey } from "@mui/material/colors"
 export default function Headers(){
     return(
         <>
-        <Typography sx={{px:5, py:2 ,color:'white'}} variant="h2">Hello there!</Typography>
-        <Typography sx={{px:5, color:blueGrey[100]}} variant="h4">Here are your notes!</Typography>
+        <Typography sx={{px:5, py:2 ,color:'white'}} variant="h2">NoteApp</Typography>
+        <Typography sx={{px:5, color:blueGrey[200]}} variant="h4">Add new notes!</Typography>
         </>
     )
 }
